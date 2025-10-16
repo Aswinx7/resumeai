33d22cf3b176bfeb517ae331119b278cb7a255d8
Aswin <aswinrajr12@gmail.com>
2025-10-17 03:03:22 +0530

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./resume builder/index.html",
+//     "./resume builder/src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./resume builder/index.html",
-    "./resume builder/src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
